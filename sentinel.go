9c607a12cd6094bf98b999d8c82b3f387850be0f
hjd <hjd_2008@foxmail.com>
2018-12-18 18:40:09 +0800

    update sentinel store

--- sentinel.go
+++ sentinel.go
@@ -34,20 +34,20 @@ func redisConnFunc(network,addr string) (radix.Conn, error)  {
 	c,err:=radix.Dial(network,addr,radix.DialAuthPass(MyRedisConf.Password),radix.DialSelectDB(MyRedisConf.DB),
 		radix.DialTimeout(time.Millisecond * time.Duration(MyRedisConf.TimeOut)))
 	if err!=nil{
-		Logger.Logger.Errorln("redis conn dial 报错！！！err:",err)
+		Logger.Errorln("redis conn dial 报错！！！err:",err)
 		return c,err
 	}
 	var ping_result string
 	err=c.Do(radix.Cmd(&ping_result,"ping"))
 	if err!=nil{
-		Logger.Logger.Errorln("redis conn do ping 报错！！！err:",err)
+		Logger.Errorln("redis conn do ping 报错！！！err:",err)
 		return c,err
 	}
 	if ping_result!="PONG"{
-		Logger.Logger.Errorln("redis conn do ping 没有受到PONG！！！ping_result:",ping_result)
+		Logger.Errorln("redis conn do ping 没有受到PONG！！！ping_result:",ping_result)
 		return c,errors.New(fmt.Sprintln("redis conn do ping 没有受到PONG！！！ping_result:",ping_result))
 	}
-	Logger.Logger.Debugln("连接 redis 成功！！！")
+	Logger.Debugln("连接 redis 成功！！！")
 	return c,nil
 }
 
@@ -55,7 +55,7 @@ func redisPoolFunc(network,addr string) (radix.Client,error)  {
 
 	p,err:=radix.NewPool(network,addr,MyRedisConf.Pool,radix.PoolConnFunc(redisConnFunc),radix.PoolOnFullClose())
 	if err!=nil{
-		Logger.Logger.Errorln("redis pool dial 报错！！！errr:",err)
+		Logger.Errorln("redis pool dial 报错！！！errr:",err)
 		return p,err
 	}
 	return p,nil
